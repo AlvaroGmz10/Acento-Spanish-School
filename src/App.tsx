b2eacd823520326459c7c49299f55277d2723894
Alvaro <a2004gn@gmail.com>
2026-04-26 00:02:36 -0500

    Add Analytics and Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Home from "./componentes/Home";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
     <>
       <Home />
+      <Analytics />
+      <SpeedInsights />
     </>
   );
 }
